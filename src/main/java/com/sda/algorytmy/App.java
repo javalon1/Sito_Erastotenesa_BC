0c183988352ce2651bd33e60d05e32bcc750dd2e
Bartlomiej Cyba <bartlomiejcyba@gmail.com>
2019-05-18 12:52:59 +0100

    wersja podstawowa

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,12 +2,25 @@ package com.sda.algorytmy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+        int k = 2;
+        while (k < 200) {
+            int licznik = 0;
 
+            for (int i = 2; i < k; i++) {
+                if (k % i == 0) {
+                    // System.out.println("Jest dzielnik:" + i);
+                    licznik++;
+                }
+            }
+            if (licznik == 0) {
+                System.out.println(k + " jest liczba pierwsza ");
+            } else {
+                System.out.println(k + " nie jest liczba pierwsza ");
+            }
+            k++;
+        }
     }
 }
